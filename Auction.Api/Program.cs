534ccb04fe8ff1908ee01bb8dc89b7dc6db0978f
rhogger <rhoggerrv@gmail.com>
2024-06-10 23:16:59 -0300

    :bug: fix: Tentativa de correção para autenticação do KeyVault

--- Auction.Api/Program.cs
+++ Auction.Api/Program.cs
@@ -1,9 +1,13 @@
 using Auction.Api;
 using Auction.Api.Common.Api;
 using Auction.Api.Endpoints;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddKeyVault();
+builder.Configuration.AddAzureKeyVault(
+  new Uri("https://auction-keyvault.vault.azure.net/"),
+  new DefaultAzureCredential()
+);
 builder.AddConfiguration();
 builder.AddDataContext();
 builder.AddCrossOrigin();
